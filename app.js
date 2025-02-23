2667241453d769007668a190542d4e7a42931227
harsh432004 <harshvaidya432004@gmail.com>
2025-02-23 20:05:36 +0530

    exploring the get post put routes

--- app.js
+++ app.js
@@ -3,20 +3,39 @@ require('dotenv').config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send('Hello from Main Page!');
-});
+
 
 app.get("/home", (req, res) => {
     res.send('Hello from Home Page!');
+});
+
+app.post("/home", (req, res) => {
+    res.send('Adding your data to db!');
+    console.log('Data added to db');
+});
+
+app.delete("/home", (req, res) => {
+    res.send('Deleting your data from db!');
+    console.log('Data deleted from db');
+});
+
+app.post("/api/login", (req, res) => {
+    console.log("Added login");
+    res.send('Hello from Login Page!');
 });
 
 app.get("/api", (req, res) => {
     res.send('Hello from API Page!');
 });
 
+
+
 app.get("/about-us", (req, res) => {
     res.send('Hello from About-Us Page!');
+});
+
+app.get("/", (req, res) => {
+    res.send('Hello from Main Page!');
 });
 
 const port = process.env.PORT || 3000;
